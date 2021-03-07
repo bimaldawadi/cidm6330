e6f591166d53e3d2c5ec25da42e1aba9ed55df2b
bimald24 <bimaldawadi.24@gmail.com>
2021-03-07 16:48:10 -0600

    Added test cases for database.py

--- Barky/Test_database.py
+++ Barky/Test_database.py
@@ -1,4 +1,9 @@
 import unittest
+import os
+from datetime import datetime
+import sqlite3
+import pytest
+
 from database import DatabaseManager
 
 from unittest.mock import Mock
@@ -9,6 +14,119 @@ class testDatabaseManager(unittest.TestCase):
         values = Mock
         expected = Mock
         self.assertEqual(expected, DatabaseManager._execute(statement, values))
+
+        pytest.fixture
+    def database_manager() -> DatabaseManager:
+        filename = "test_bookmarks.db"
+        dbm = DatabaseManager(filename)
+        yield dbm
+        dbm.__del__() 
+        os.remove(filename)
+
+    def test_database_manager_create_table(database_manager):
+        database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+        conn = database_manager.connection
+        cursor = conn.cursor()
+
+        cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bookmarks' ''')
+
+        assert cursor.fetchone()[0] == 1
+    
+        database_manager.drop_table("bookmarks")
+
+    def test_database_manager_add_bookmark(database_manager):
+        database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+    # act
+    database_manager.add("bookmarks", data)
+
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')    
+    assert cursor.fetchone()[0] == 1
+
+    def test_database_manager_delete_bookmark(database_manager):
+        database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title_delete",
+        "url": "http://example.com",
+        "notes": "test notes delete",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+   
+    database_manager.add("bookmarks", data)
+
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' DELETE * FROM bookmarks WHERE title='test_title_delete' ''')    
+    assert cursor.fetchone()[0] == 1 
+
+
+    def test_database_manager_select_bookmark(database_manager):
+        database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title_select",
+        "url": "http://example.com",
+        "notes": "test notes select",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+   
+    database_manager.add("bookmarks", data)
+
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title_select' ''')    
+    assert cursor.fetchone()[0] == 1 
+
 
     def testcreate_table(self):
         tablename = "BookMarks"
